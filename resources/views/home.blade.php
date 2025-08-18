2662dcc2e5435bb8192d32d4cbe1fa0bff5e9db2
Nabil Chaguer <nabil.chaguer@cybergenix.fr>
2025-08-18 20:28:27 +0200

    projet-ecf-feature-1001: creation du menu

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,6 +14,32 @@
 </head>
 <body>
 
+
+        <!-- Navbar -->
+            <nav id="navbar" class="navbar navbar-expand-lg fixed-top py-3">
+                <div class="container">
+                    <a class="navbar-brand fw-bold text-success" href="/">
+                        <i class=" bi-flower1"></i> EcoRide</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                        <div class="offcanvas-header">
+                            <h5 id="offcanvasNavbarLabel" class="fw-bold">Menu</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Fermer"></button>
+                        </div>
+                        <div class="offcanvas-body">
+                            <ul class="navbar-nav ms-auto">
+                                <li class="nav-item"><a class="nav-link" href="/">Accueil</a></li>
+                                <li class="nav-item"><a class="nav-link" href="/covoiturage">Covoiturage</a></li>
+                                <li class="nav-item"><a class="nav-link" href="/contact">Contact</a></li>
+                                <li class="nav-item"><a class="btn btn-color" href="/login">Connexion</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+
         <!-- First section -->
         <section class="py-5 bg-gradient-section">
             <div class="container text-center text-md-start mt-5">
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use Illuminate\Routing\Route as RoutingRoute;
 
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/covoiturage', [HomeController::class, 'index']);
+Route::get('/contact', [HomeController::class, 'index']);
+Route::get('/login', [HomeController::class, 'index']);
 Route::get('/commencer', [HomeController::class, 'index']);
 Route::get('/chercher-routes', [HomeController::class, 'index']);
 Route::get('mention-legale', [HomeController::class, 'index']);
